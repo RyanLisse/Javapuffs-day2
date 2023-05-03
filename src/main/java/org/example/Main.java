b984ed98297e6b995e037e8a9099e9d1c7651fea
RyanLisse <57917217+RyanLisse@users.noreply.github.com>
2023-05-03 17:03:26 +0200

    Added first test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,46 @@
 package org.example;
 
+import java.text.ParseException;
+
+import java.time.LocalDate;
+
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+import java.util.Scanner;
+
+//Create an application that takes a birthdate and calculates the age in days
 public class Main {
+
+    static LocalDate now = LocalDate.now();
+
     public static void main(String[] args) {
 
-        
+        //ask user for their birthday and get their input
+        System.out.println("Enter your birthdate (dd-mm-yyyy): ");
+        Scanner scanner = new Scanner(System.in);
+        String birthdate = scanner.next();
+        scanner.close();
+
+        LocalDate date = convertBirthDate(birthdate);
+        getNumberOfDaysOnEarth(date, now);
     }
+
+    public static long getNumberOfDaysOnEarth(LocalDate date, LocalDate now) {
+        //calculate days between user birthdate and current date
+        long diffInDays = ChronoUnit.DAYS.between(date, now);
+        System.out.println("Your age in days is: " + diffInDays);
+        return diffInDays;
+    }
+
+    public static LocalDate convertBirthDate(String birthdate) {
+        //convert the user inputted birthdate to LocalDate
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate date = LocalDate.parse(birthdate, dtf);
+        return date;
+    }
+
+
+
+
 }
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,23 @@
+package org.example;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+class AClassWithOneJUnitTest {
+
+    @Test
+    void shouldReturnRightAmountOfDays() {
+        long days = Main.getNumberOfDaysOnEarth(Main.convertBirthDate("09-03-1984"), LocalDate.now());
+        assertEquals(14299,days);
+    }
+@Test
+    void shouldInputExistingBirthdate() {
+        LocalDate now = LocalDate.now();
+        LocalDate birthdate = Main.convertBirthDate("12-12-2025");
+        assertTrue(birthdate.isBefore(now));
+    }
+    
+}
